9d7bf9c6f986bbb462fe84d79650358e23c3c9ea
Franz Geffke <franz@pantherx.org>
2023-08-04 16:16:40 +0100

    adapt client to recent changes

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,4 @@
-import { NewFilters, logRelayMessage } from "@nostr-ts/common";
+import { NFilters, logRelayMessage } from "@nostr-ts/common";
 import { loadOrCreateKeypair, RelayClient } from "@nostr-ts/node";
 
 const main = async () => {
@@ -10,7 +10,7 @@ const main = async () => {
     "wss://nostr.lu.ke",
   ]);
 
-  const filters = new NewFilters();
+  const filters = new NFilters();
   // filters.addAuthor(keypair.pub)
   // filters.addKind(6)
 
@@ -25,37 +25,45 @@ const main = async () => {
     logRelayMessage(payload.data);
   });
 
-  // SEND A MESSAGE
-  // const note = NewShortTextNote('Hello nostr #3!')
-  // note.signAndGenerateId(keypair)
-  // client.sendEvent(note)
+  // // SEND A MESSAGE
+  // const note = NewShortTextNote({
+  //   text: "Hello nostr #3!",
+  // });
+  // note.signAndGenerateId(keypair);
+  // client.sendEvent(note);
 
-  // SEND REACTION
-  // const reaction = NewReaction('+', {
-  //     id: 'e21921600ecbcbea699a9f76c8156886bef112b71c4f79ce1b894386b5413466',
-  //     pubkey: '5276ac499c9c6a353634d3d2cb6f4ada5167c3b886108ab4ddeb8ddf7b0fff70',
-  // })
+  // // SEND REACTION
+  // const reaction = NewReaction({
+  //   text: "+",
+  //   inResponseTo: {
+  //     id: "e21921600ecbcbea699a9f76c8156886bef112b71c4f79ce1b894386b5413466",
+  //     pubkey:
+  //       "5276ac499c9c6a353634d3d2cb6f4ada5167c3b886108ab4ddeb8ddf7b0fff70",
+  //   },
+  // });
 
-  // reaction.signAndGenerateId(keypair)
-  // const verified = verifyEvent(reaction)
-  // console.log(`Verified: ${verified}`)
-  // client.sendEvent(reaction)
+  // reaction.signAndGenerateId(keypair);
+  // const verified = verifyEvent(reaction);
+  // console.log(`Verified: ${verified}`);
+  // client.sendEvent(reaction);
 
-  // SEND REPOST
-  // const repost = NewQuoteRepost(
-  //     'https://nostr.rocks',
-  //     {
-  //         id: 'e21921600ecbcbea699a9f76c8156886bef112b71c4f79ce1b894386b5413466',
-  //         pubkey: '5276ac499c9c6a353634d3d2cb6f4ada5167c3b886108ab4ddeb8ddf7b0fff70',
-  //         created_at: 1690881792,
-  //         kind: 1,
-  //         tags: [],
-  //         content: "Hello everyone! I am working on a new ts library for nostr. This is just a test.",
-  //         sig: '6cee8c1d11ca5f8c7a0bd9839d0af5d3af3cc6a5de754fc449d34188c0066eee3e5b5b4e567cd77a2e0369f8c9525d60e064db175acd02d9c5374c3c0e912969'
-  //     }
-  // )
-  // repost.signAndGenerateId(keypair)
-  // client.sendEvent(repost)
+  // // SEND REPOST
+  // const repost = NewQuoteRepost({
+  //   relayUrl: "https://nostr.rocks",
+  //   inResponseTo: {
+  //     id: "e21921600ecbcbea699a9f76c8156886bef112b71c4f79ce1b894386b5413466",
+  //     pubkey:
+  //       "5276ac499c9c6a353634d3d2cb6f4ada5167c3b886108ab4ddeb8ddf7b0fff70",
+  //     created_at: 1690881792,
+  //     kind: 1,
+  //     tags: [],
+  //     content:
+  //       "Hello everyone! I am working on a new ts library for nostr. This is just a test.",
+  //     sig: "6cee8c1d11ca5f8c7a0bd9839d0af5d3af3cc6a5de754fc449d34188c0066eee3e5b5b4e567cd77a2e0369f8c9525d60e064db175acd02d9c5374c3c0e912969",
+  //   },
+  // });
+  // repost.signAndGenerateId(keypair);
+  // client.sendEvent(repost);
 
   // delay by 10800 seconds client.closeConnection()
   await new Promise((resolve) => setTimeout(resolve, 1 * 60 * 1000)).then(

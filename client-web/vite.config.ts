acc632e5eaf195cc6708bc1f32a1bcc2b46822ad
Franz Geffke <franz@pantherx.org>
2023-08-21 18:55:59 +0100

    github workflow [4]

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-const BASE_URL = process.env.CLIENT_WEB_BASE_URL || "/";
+const BASE_URL = process.env.VITE_CLIENT_WEB_BASE_URL || "/";
 
 export default defineConfig({
   base: BASE_URL,

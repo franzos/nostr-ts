ddcc6d1f9cdd6550fc25baf7c743860947cf99e9
Franz Geffke <franz@pantherx.org>
2023-08-21 11:53:29 +0100

    github workflow: adjust base url [2]

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: process.env.GH_PAGES_BUILD ? "/nostr-ts/" : "/", // for github pages
   plugins: [react()],
 });

2a15503856c00d0987660c0a41de64e65ddd76a7
Franz Geffke <franz@pantherx.org>
2023-09-29 23:23:00 +0100

    client-web: tweak link preview

--- client-web/src/components/event/link-preview.tsx
+++ client-web/src/components/event/link-preview.tsx
@@ -10,11 +10,14 @@ import { useEffect, useState } from "react";
 import { excerpt } from "../../lib/excerpt";
 
 const fetchProxyData = async (url: string) => {
-  const response = await fetch(`https://p1.nostrop.com?url=${url}`, {
-    headers: {
-      client: "nostrop",
-    },
-  });
+  const response = await fetch(
+    `https://p1.nostrop.com/fea11fdd-6073-4a6f-b45a-31559c41f9f1?url=${url}`,
+    {
+      headers: {
+        client: "nostrop",
+      },
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
